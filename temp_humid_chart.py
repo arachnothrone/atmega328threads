74827cbcd5a67dd0462cc6268b59dc6c4ed558d8
arachnothrone <js1000avf@gmail.com>
2019-06-16 19:29:19 -0400

    Graph improvements

--- temp_humid_chart.py
+++ temp_humid_chart.py
@@ -45,15 +45,37 @@ if __name__ == "__main__":
     #         t.pop(i)
     #         h.pop(i)
     #         z.pop(i)
-    t = t[::100]
-    h = h[::100]
-    z = z[::100]
+
+    # reduce the number of points, take every 20th, and convert elements from String to float/int
+    t = [float(x) for x in t[::20]]
+    h = [float(x) for x in h[::20]]
+    z = [int(x) for x in z[::20]]
+
     outputFile = open("temphumid.log", 'w')
     outputFile.write("Time, temperature C, humidity %\n")
     for (tt, hh, zz) in zip(t, h, z):
         outputFile.write("{}, {}, {}\n".format(zz, tt, hh))
+        print("time: {0:10}  temp = {1:3.1f}  humid = {2:3.1f}".format(zz, tt, hh))
     outputFile.close()
     #print(plotData)
     #print(len(t), len(h), len(z))
-    plt.plot(z, h)
+    # plt.plot(z, h)
+    # plt.show()
+
+    # Plot the graphs
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.plot(z, t, 'b-', label='temperature, C')
+    ax1.set_xlabel('time, seconds', color='grey')
+
+    ax2 = ax1.twinx()
+    ax2.plot(z, h, 'r-', label='humidity, %')
+
+    fig.legend()
+
+    for tl in ax1.get_yticklabels():
+        tl.set_color('b')
+    for tl in ax2.get_yticklabels():
+        tl.set_color('r')
+
     plt.show()
